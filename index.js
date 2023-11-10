d68aa14a0e09effaccc16e25eeaac0b187e3c841
Jakub Novak <jakub@e2b.dev>
2023-11-10 19:11:05 +0100

    Implement git functions

--- index.js
+++ index.js
@@ -6,16 +6,25 @@ import OpenAI from 'openai'
 const openai = new OpenAI()
 // const sandbox = await Sandbox.create({ id: 'ai-developer-sandbox' })
 
-async function cloneRepo(repoURL) {
-	console.log('Clone repo')
+async function cloneRepo(sandbox, repoURL) {
+	const process = await sandbox.process.start(`git clone ${repoURL}`)
+	await process.wait()
+	const processCreateBranch = await sandbox.process.start('git checkout -b ai-developer')
+	await processCreateBranch.wait()
 }
 
-async function makeCommit(message) {
-	console.log('Make commit')
+async function makeCommit(sandbox, message) {
+	const processAdd =await sandbox.process.start('git add .')
+	await processAdd.wait()
+	const processCommit = await sandbox.process.start(`git commit -m "${message}"`)
+	await processCommit.wait()
 }
 
-async function makePullRequest() {
-	console.log('Make pull request')
+async function makePullRequest(sandbox, title) {
+	const processPush = await sandbox.process.start('git push')
+	await processPush.wait()
+	const processPR = await sandbox.process.start(`gh pr create --title "${title}"`)
+	await processPR.wait()
 }
 
 async function saveCodeToFile(sandbox, code, filename) {
