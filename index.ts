bbc38d5082027da3f76f2f17934d98833d9438f7
Jakub Novak <jakub@e2b.dev>
2023-11-14 18:37:21 +0100

    Improve error messages

--- index.ts
+++ index.ts
@@ -38,34 +38,33 @@ function sleep(time: number) {
 }
 
 
-async function loginWithGH(sandbox: Sandbox): Promise<string> {
+async function loginWithGH(sandbox: Sandbox) {
 	await sandbox.filesystem.write('/home/user/.github-token', GITHUB_TOKEN)
 	const process = await sandbox.process.start({ cmd: `gh auth login --with-token < /home/user/.github-token &&
 git config --global user.email '${gitEmail}' &&
 git config --global user.name '${gitName}' &&
 git config --global push.autoSetupRemote true`})
-	await process.wait()
-
-	if (process.output.stderr) {
-		return `fail: ${process.output.stderr}`
+	const output = await process.wait()
+	if (output.exitCode !== 0) {
+		throw new Error(`Make sure you've set 'GITHUB_TOKEN' env variable correctly. ${output.stderr}`)
 	}
-
-	return 'success'
 }
 
 async function cloneRepo(sandbox: Sandbox, repo: string) {
 	sandboxLog(`Cloning repo ${repo}`)
 
 	const process = await sandbox.process.start({ cmd: `gh repo clone ${repo} ${repoDirPath}` })
-	await process.wait()
+	const output = await process.wait()
+
+	if (output.exitCode !== 0) {
+		throw new Error(`Cloning repo failed. ${output.stderr}`)
+	}
 
 	const processCreateBranch = await sandbox.process.start({ cmd: `git checkout -b ai-developer-${branchID}`, cwd: repoDirPath })
 	await processCreateBranch.wait()
 
 	const setRemote = await sandbox.process.start({ cmd: `git remote set-url origin https://${GIT_USERNAME}:${GITHUB_TOKEN}@github.com/${repo}.git`, cwd: repoDirPath })
 	await setRemote.wait()
-
-	return 'success'
 }
 
 async function makeCommit(sandbox: Sandbox, message: string): Promise<string> {
